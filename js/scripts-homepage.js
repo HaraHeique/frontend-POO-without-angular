e0d54906465cc4758a783c3976cc7d19283d339e
Harã Heique dos Santos <heikacademicos@hotmail.com>
2018-10-21 18:02:32 -0300

    Criação da estrutura e página homepage

---
+++ js/scripts-homepage.js
@@ -0,0 +1,39 @@
+/* Para a mudança das imagens na homepage */
+
+document.addEventListener('DOMContentLoaded', function() {
+    var infoMiniaturaPopulacao = document.getElementById('info-miniatura-populacao');
+    var miniaturaPopulacao = document.getElementById('miniatura-populacao');
+    var msgPopulacao = "Voltado para população";
+
+    var infoMiniaturaAtendente = document.getElementById('info-miniatura-atendente');
+    var miniaturaAtendente = document.getElementById('miniatura-atendente');
+    var msgAtendente = "Voltado para os atendentes dos postos";
+    
+    miniaturaPopulacao.addEventListener('mouseover', function(e) {
+        putBodyImg(infoMiniaturaPopulacao, msgPopulacao, "../img/atendimento-populacao3.jpg");
+    });
+
+    miniaturaAtendente.addEventListener('mouseover', function(e) {
+        putBodyImg(infoMiniaturaAtendente, msgAtendente, "../img/controle-estoque2.jpg");
+    });
+
+    miniaturaPopulacao.addEventListener('mouseleave', function(e) {
+        putOffBodyImg(infoMiniaturaPopulacao, "../img/care-health.jpg");
+    });
+
+    miniaturaAtendente.addEventListener('mouseleave', function(e) {
+        putOffBodyImg(infoMiniaturaAtendente, "../img/care-health.jpg");
+    });
+});
+
+// Troca a imagem quando passa o mouse sobre a blackbox da imagem
+function putBodyImg(infoMiniatura, msg, pathImg) {
+    infoMiniatura.innerHTML = msg;
+    document.body.style.backgroundImage = "url(" + pathImg + ")";
+}
+
+// Troca para imagem padrão quando o mouse sai do blackbox da imagem
+function putOffBodyImg(infoMiniatura, pathImgPaddern) {
+    infoMiniatura.innerHTML = "";
+    document.body.style.backgroundImage = "url(" + pathImgPaddern + ")";
+}
