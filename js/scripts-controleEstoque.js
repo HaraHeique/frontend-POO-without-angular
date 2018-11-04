d9d566e19871cf99c98d5b97fa3b7369e0c695ab
Harã Heique dos Santos <heikacademicos@hotmail.com>
2018-11-04 01:35:51 -0200

    Criação das páginas de controle de estoque

--- js/scripts-controleEstoque.js
+++ js/scripts-controleEstoque.js
@@ -116,5 +116,53 @@ function clickedButtonNavEvent(groupButtons) {
     });
 }
 
+function renderHtmlHeaderShared() {
+    var html = '';
+    
+    html += '<div class="sign-out">';
+    html += '   <a href="index.html">';
+    html += '       <i title="Logout" class="fa fa-sign-out" aria-hidden="true"></i>';
+    html += '   </a>';       
+    html += '</div>';
+
+    document.getElementById('header').insertAdjacentHTML('afterbegin', html);
+}
+
+function renderHtmlNavShared() {
+    var html = '';
+
+    html += '<nav>';
+    html += '   <div class="container-fluid">';
+    html += '       <div class="row">';
+    html += '           <div class="btn-group btn-navigation col-sm-12 mb-1" role="group">';
+    html += '               <a href="cadastro-medicamentos.html" class="btn btn-default">Cadastrar Medicamentos</a>';
+    html += '               <a href="cadastro-pacientes.html" class="btn btn-default">Cadastrar Pacientes</a>';
+    html += '               <a href="retirar-medicamentos.html" class="btn btn-default">Retirar Medicamentos Solicitados</a>';
+    html += '           </div>';
+    html += '       </div>';
+    html += '       <div class="row">';
+    html += '           <div class="btn-group btn-navigation offset-sm-1 col-sm-11" role="group">';
+    html += '               <a href="solicitacao-medicamentos.html" class="btn btn-default">Solicitação de Medicamentos</a>';
+    html += '               <a href="recebimento-medicamentos.html" class="btn btn-default">Recebimento de Medicamentos</a>';
+    html += '               <a href="gerar-relatorios.html" class="btn btn-default">Gerar Relatórios</a>';
+    html += '           </div>';
+    html += '       </div>';
+    html += '   </div>';
+    html += '</nav>';
+
+    document.getElementById('navigation').insertAdjacentHTML('afterbegin', html);
+}
+
+function rederHtmlFooterShared() {
+    var html = '';
+
+    document.getElementById('footer').insertAdjacentHTML('afterbegin', html);
+}
+
+function changeColorButtonNav(btnNavClicked) {
+    btnNavClicked.classList.remove('btn-default');
+    btnNavClicked.classList.add('btn-primary');
+    btnNavClicked.href = '#';
+}
 
     
